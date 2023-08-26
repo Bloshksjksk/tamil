3b08e5a02e6035c4c7146fc2f13b104a7391aede
LieStage <118051769+LieStage@users.noreply.github.com>
2023-08-26 13:36:46 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -419,7 +419,7 @@ async def filter_languages_cb_handler(client: Client, query: CallbackQuery):
                 InlineKeyboardButton("Quality👁",  callback_data=f"quality#{key}")
             ]
         )
-         btn.insert(0,
+        btn.insert(0,
             [
                   InlineKeyboardButton("Quality👁",  callback_data=f"quality#{key}")  
             ]
